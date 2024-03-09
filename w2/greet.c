ce765f54fe019de3ad556e36dadb5d7f7bb296b0
zsun-ubuntu <nakani2623@outlook.com>
2024-03-10 01:06:32 +1100

    added names.txt, now I know more about files

--- w2/greet.c
+++ w2/greet.c
@@ -6,9 +6,15 @@ int main(int argc, char **argv)
         return 1;
     char *msg = argv[1];
     printf("What is your name? ");
+
     char buffer[20];
     fgets(buffer, 20, stdin);
-    
     printf("%s, %s", msg, buffer);
+
+    // fgets will stop reading when newline is read. refer to man (3) fgets
+    // file stream will remain
+    fgets(buffer, 20, stdin);
+    printf("%s, %s", msg, buffer);
+
     return 0;
 }
