af215af93c8f8c08eaf3a0f46fce7dad728f2092
zsun-ubuntu <nakani2623@outlook.com>
2024-03-10 00:51:06 +1100

    greeting, interacting with fgets

---
+++ w2/greet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) 
+{
+    if (argc < 2)
+        return 1;
+    char *msg = argv[1];
+    printf("What is your name? ");
+    char buffer[20];
+    fgets(buffer, 20, stdin);
+    
+    printf("%s, %s", msg, buffer);
+    return 0;
+}
